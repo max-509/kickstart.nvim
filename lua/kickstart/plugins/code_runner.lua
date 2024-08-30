5451fd2c1b65f0f5b3ba8a599fccb3127403221b
Vershinin Maxim 00873208 <vershinin.maxim1@huawei.com>
2024-08-30 15:40:02 +0700

    Update

--- lua/kickstart/plugins/code_runner.lua
+++ lua/kickstart/plugins/code_runner.lua
@@ -3,6 +3,24 @@ return {
   dependencies = { "ThePrimeagen/harpoon" },
   config = function()
     require('code_runner').setup({
+      float = {
+        close_key = "<ESC>",
+        -- Window border (see ':h nvim_open_win')
+        border = "none",
+
+        -- Num from `0 - 1` for measurements
+        height = 0.8,
+        width = 0.8,
+        x = 0.5,
+        y = 0.5,
+
+        -- Highlight group for floating window/border (see ':h winhl')
+        border_hl = "FloatBorder",
+        float_hl = "Normal",
+
+        -- Transparency (see ':h winblend')
+        blend = 0,
+      },
       filetype = {
         java = {
           "cd $dir &&",
@@ -32,16 +50,18 @@ return {
             require("code_runner.commands").run_from_fn(vim.list_extend(c_base, c_exec))
           end)
         end,
+        sh = "./$fileName",
+        go = "go run",
       },
     })
 
-    vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { desc = "[R]un Code", noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { desc = "[R]unFile", noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { desc = "[R]un File Tab", noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { desc = "[R]un Project", noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { desc = "[R]un Close", noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { desc = "C[R]Filetype", noremap = true, silent = false })
-    vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { desc = "C[R]Projects", noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>Rc', ':RunCode<CR>', { desc = "[R]un Code", noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>Rf', ':RunFile<CR>', { desc = "[R]un File", noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>Rft', ':RunFile tab<CR>', { desc = "[R]un File Tab", noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>Rp', ':RunProject<CR>', { desc = "[R]un Project", noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>RC', ':RunClose<CR>', { desc = "[R]un Close", noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>CRf', ':CRFiletype<CR>', { desc = "C[R]Filetype", noremap = true, silent = false })
+    vim.keymap.set('n', '<leader>CRp', ':CRProjects<CR>', { desc = "C[R]Projects", noremap = true, silent = false })
 
     -- require("harpoon.term").sendCommand(1, require("code_runner.commands").get_filetype_command() .. "\n")
   end
